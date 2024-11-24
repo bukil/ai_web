9f5e1093eb7fb9a0560cf653fc185a051adf8055
Prince_Afdob <prince289446@gmail.com>
2024-11-24 18:21:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@
 
     # ===================================================================
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import requests
 import base64
